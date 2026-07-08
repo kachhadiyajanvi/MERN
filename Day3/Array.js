d3fcb0e0473def56722c8b4813c1505651466db5
janvikachhadiya <janvikachhadiya1@gmail.com>
2026-07-08 11:19:39 +0530

    Assignment and day 4 commit

--- Day3/Array.js
+++ Day3/Array.js
@@ -1,33 +1,34 @@
 let arr=[1,2,"janvi",5,6]
-console.log(arr.length);
+// console.log(arr.length);
 
-console.log(typeof arr);
-console.log(typeof arr)
-console.log(arr instanceof Array)
-console.log(arr instanceof String)
-console.log(arr[3])
-console.log(arr[6])
-console.log(arr[-1])
-console.log(arr)
+// console.log(typeof arr);
+// console.log(typeof arr)
+// console.log(arr instanceof Array)
+// console.log(arr instanceof String)
+// console.log(arr[3])
+// console.log(arr[6])
+// console.log(arr[-1])
+// console.log(arr)
 
-for (let i=0;i<arr.length;i++){
-    console.log(arr[i])
-}
+// for (let i=0;i<arr.length;i++){
+//     console.log(arr[i])
+// }
 
-for (let val of arr){
-    console.log(val)
-}
+// for (let val of arr){
+//     console.log(val)
+// }
 
-arr.push(102,56)
-console.log(arr)
-arr.unshift(8,7,6)
-arr.pop()
-arr.shift()
-console.log(arr)
-arr.splice(2,1,20,30,40,50)
-console.log(arr)
-console.log(arr.slice(4))
-console.log(arr.slice(-4))
-console.log(arr.slice(-4,-2))
-console.log(arr.slice(-2,-4))
+// arr.push(102,56)
+// console.log(arr)
+// arr.unshift(8,7,6)
+// arr.pop()
+// arr.shift()
+// console.log(arr)
+// arr.splice(2,1,20,30,40,50)
+// console.log(arr)
+// console.log(arr.slice(4))
+// console.log(arr.slice(-4))
+// console.log(arr.slice(-4,-2))
+// console.log(arr.slice(-2,-4))
 
+console.log(arr.sort())
